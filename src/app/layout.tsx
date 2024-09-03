993df9bf86a3923ddfcfb0f744124d469e3a74e9
cyteon <cyteon>
2024-09-03 18:40:30 +0200

    metadata no more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,6 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "CDN",
-  description: "Another CDN",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
